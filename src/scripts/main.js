e75b72a8f481ab79dcb7d060aed8e7209c858f0d
Nazar Kundys <nazarrisd@gmail.com>
2025-12-27 22:01:00 +0200

    ✨ feat: implement form label generation
    - Add labels to form fields using `humanizeName()` function
    - Set input placeholders with capitalized field names
    - Update demo link in **README**

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('form .field').forEach((field) => {
+  const inputEl = field.querySelector('input');
+  const inputName = humanizeName(inputEl.name);
+
+  field.insertAdjacentHTML(
+    'afterbegin',
+    `<label class="field-label" for="${inputEl.id}">${inputName}</label>`,
+  );
+
+  inputEl.placeholder = inputName[0].toUpperCase() + inputName.slice(1);
+});
+
+function humanizeName(str) {
+  return str.replace(/([A-Z])/g, ' $1');
+}
